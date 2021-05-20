c484623f81223812bc3d20c84bd2ea6d377a3cad
Soumya Ray <soumya.ray@gmail.com>
2021-05-20 12:13:18 +0800

    Production DB enabled

--- app/services/add_collaborator_to_project.rb
+++ app/services/add_collaborator_to_project.rb
@@ -3,12 +3,17 @@
 module Credence
   # Add a collaborator to another owner's existing project
   class AddCollaboratorToProject
+    # Error for owner cannot be collaborator
+    class OwnerNotCollaboratorError < StandardError
+      def message = 'Owner cannot be collaborator of project'
+    end
+
     def self.call(email:, project_id:)
       collaborator = Account.first(email: email)
       project = Project.first(id: project_id)
-      return false if project.owner.id == collaborator.id
+      raise(OwnerNotCollaboratorError) if project.owner.id == collaborator.id
 
-      project.add_collaborator
+      project.add_collaborator(collaborator)
       collaborator
     end
   end
